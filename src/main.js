e998bde9eeefeeb138ac90f9d165495289f28ec9
lijiaze <1013285478@qq.com>
2019-05-14 11:04:36 +0800

    VueProject initial

--- src/main.js
+++ src/main.js
@@ -3,9 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
-
+// 解决移动端点击300ms延迟
+fastClick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
