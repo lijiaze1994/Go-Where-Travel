5720f4ffc38b0fae2962948f99cf23da59ed0d37
lijiaze <1013285478@qq.com>
2019-05-20 10:32:20 +0800

    add swiper

---
+++ src/components/Home/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont icon-back">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题
+        </div>
+        <div class="header-right">
+            城市
+            <span class="iconfont">&#xe6aa;</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+.header
+    line-height: 43px
+    display: flex
+    background: $backgroundColor
+    color: #fff
+.header-left
+    width: 38px
+    float: left
+    text-align: center
+.icon-back
+    text-align: center
+    font-size: 20px
+.header-input
+    flex: 1
+    margin-top: 6px
+    height:31px
+    line-height: 31px
+    font-size: 13px
+    margin-left: 2px
+    padding-left: 4px
+    background: #fff
+    border-radius: 5px
+    color: #ccc
+.header-right
+    width: 64px
+    float: right
+    text-align: center
+    font-size: 15px
+</style>
---
+++ src/components/Home/components/Swiper.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="item of swiperList" v-bind:key="item.id">
+            <img class="swiper-img" v-bind:src='item.imgUrl' />
+        </swiper-slide>
+     <!--    <swiper-slide>
+            <img class="swiper-img" src="~images/swiper2.jpg" />
+        </swiper-slide>
+        <swiper-slide>
+            <img class="swiper-img" src="~images/swiper3.jpg" />
+        </swiper-slide>
+        <swiper-slide>
+            <img class="swiper-img" src="~images/swiper4.jpg" />
+        </swiper-slide> -->
+        <!-- Optional controls -->
+        <div class="swiper-pagination"  slot="pagination"></div>
+      <!--   <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div> -->
+     <!--    <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
+      </swiper>
+    </div>
+</template>
+<script type="text/javascript">
+export default {
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        speed: 1500
+      },
+      swiperList: [{
+        id: '01',
+        imgUrl: require('../../../assets/images/swiper.jpg')
+      }, {
+        id: '02',
+        imgUrl: require('../../../assets/images/swiper2.jpg')
+      }, {
+        id: '03',
+        imgUrl: require('../../../assets/images/swiper3.jpg')
+      }, {
+        id: '04',
+        imgUrl: require('../../../assets/images/swiper4.jpg')
+      }]
+    }
+  }
+}
+</script>
+<style lang='stylus' scoped>
+.wrapper
+    width: 100%
+    height: 0
+    overflow: hidden
+    padding-bottom: 28%
+.swiper-img
+    width: 100%
+.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+</style>
