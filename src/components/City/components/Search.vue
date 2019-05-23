af12d176da72584e87032fc60d6e06c8dbb8ca2c
lijiaze <1013285478@qq.com>
2019-05-23 15:28:45 +0800

    city search finish

--- src/components/City/components/Search.vue
+++ src/components/City/components/Search.vue
@@ -1,12 +1,61 @@
 <template>
-    <div class="search">
-        <input class="search-input" type="text" placeholder="输入城市名或拼音" name="">
+    <div>
+        <div class="search">
+            <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音" name="">
+        </div>
+        <div class="search-content" ref="search" v-show="keyword">
+            <ul>
+                <li class="search-item border-bottom" v-for="item of list" :key="item">{{item}}</li>
+                <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配城市</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script type="text/javascript">
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearchr'
+  name: 'CitySearchr',
+  props: {
+    cities: Array
+  },
+  data: function () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].list.forEach((value) => {
+            if (value.name.indexOf(this.keyword) > -1 || value.pinyin.toLowerCase().indexOf(this.keyword.toLowerCase()) > -1) {
+              result.push(value.name)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
@@ -26,4 +75,20 @@ export default {
     font-size: 14px
     padding: 0 5px
     box-sizing: border-box
+.search-content
+    overflow: hidden
+    position: absolute
+    top: 79px
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    z-index: 999
+.search-item{
+    line-height: 28px
+    padding-left: 10px
+    font-size: 12px
+    color: #666
+    background: #fff
+}
 </style>
